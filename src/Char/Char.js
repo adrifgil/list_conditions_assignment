8d5c63cd316f6303b50eeeebf05596d1b0884d2a
adrifgil <adrianafgil87@gmail.com>
2020-05-19 00:40:12 -0400

    liste and delete char by char

---
+++ src/Char/Char.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const char = (props) => {
+
+    const styleChar = {
+        display: 'inline-block',
+        padding: '16px',
+        margin: '16',
+        border: '1px solid black',
+        textAlign: 'center'
+    }
+    return (
+        <div style={styleChar} onClick={props.clicked}>
+            {props.character}
+        </div>
+    );
+};
+
+export default char;
