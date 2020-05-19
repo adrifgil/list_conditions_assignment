11a7e84752791bc8ed295e8cf30591ad2da6b8b2
adrifgil <adrianafgil87@gmail.com>
2020-05-18 22:51:40 -0400

    input field and validation component

---
+++ src/Validation/Validation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Validation.css';
+
+const validation = () => (props) => {
+    return (
+      <div>
+        <p></p>
+      </div>  
+    );
+};
+
+export default validation;
+
